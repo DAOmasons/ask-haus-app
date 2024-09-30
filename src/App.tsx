c60b344ab428cafc688c9fd6ce7fcbe2114b598c
jordan <jordan.lesich@gmail.com>
2024-09-30 15:57:20 -0700

    base button style

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,97 @@
 import '@mantine/core/styles.css';
-import { Button, MantineProvider, TextInput } from '@mantine/core';
-import { theme } from './theme';
+
+import {
+  Box,
+  Button,
+  Group,
+  MantineProvider,
+  Stack,
+  Text,
+  TextInput,
+  useMantineTheme,
+} from '@mantine/core';
+import { theme as mantineTheme } from './theme';
 
 export default function App() {
   return (
-    <MantineProvider theme={theme} defaultColorScheme="dark">
-      <div>
-        App <Button>Button</Button>
-        <TextInput label="test" />
-      </div>
+    <MantineProvider theme={mantineTheme} defaultColorScheme="dark">
+      <TestUI />
     </MantineProvider>
   );
 }
+
+const TestUI = () => {
+  return (
+    <Box w="500px" mx={64} h="600px" my={32}>
+      <TestNav />
+      <div>
+        App <Button>Button</Button>
+        <TextInput label="test" mb="lg" />
+      </div>
+      <TestColors />
+    </Box>
+  );
+};
+
+const TestNav = () => {
+  const theme = useMantineTheme();
+
+  return (
+    <Group align="center" mb="lg">
+      <Text
+        mr={88}
+        fz={24}
+        c={theme.colors.steel[0]}
+        fw={300}
+        style={{ letterSpacing: '1px' }}
+      >
+        ask.haus
+      </Text>
+      <Group gap={32}>
+        <Text c={theme.colors.steel[2]}>Create</Text>
+        <Text c={theme.colors.steel[2]}>Active</Text>
+        <Text c={theme.colors.steel[2]}>My</Text>
+        <Text c={theme.colors.steel[2]}>Wallet</Text>
+      </Group>
+    </Group>
+  );
+};
+
+const TestColors = () => {
+  const theme = useMantineTheme();
+
+  return (
+    <Stack>
+      <Text fz="lg" c={theme.colors.steel[9]}>
+        Steel 9
+      </Text>
+      <Text fz="lg" c={theme.colors.steel[8]}>
+        Steel 8
+      </Text>
+      <Text fz="lg" c={theme.colors.steel[7]}>
+        Steel 7
+      </Text>
+      <Text fz="lg" c={theme.colors.steel[6]}>
+        Steel 6
+      </Text>
+      <Text fz="lg" c={theme.colors.steel[5]}>
+        Steel 5
+      </Text>
+      <Text fz="lg" c={theme.colors.steel[4]}>
+        Steel 4
+      </Text>
+      <Text fz="lg" c={theme.colors.steel[3]}>
+        Steel 3
+      </Text>
+      <Text fz="lg" c={theme.colors.steel[2]}>
+        Steel 2
+      </Text>
+      <Text fz="lg" c={theme.colors.steel[1]}>
+        Steel 1
+      </Text>
+      <Text fz="lg" c={theme.colors.steel[0]}>
+        Steel 0
+      </Text>
+    </Stack>
+  );
+};
--- src/theme.ts
+++ src/theme.ts
@@ -1,5 +1,40 @@
-import { createTheme } from '@mantine/core';
+import { Button, createTheme } from '@mantine/core';
+import buttonClasses from './styles/button.module.css';
 
 export const theme = createTheme({
+  fontFamily: 'Sora',
+  defaultRadius: 'sm',
+
+  colors: {
+    steel: [
+      '#f5f5f5',
+      '#e7e7e7',
+      '#cdcdcd',
+      '#b2b2b2',
+      '#9a9a9a',
+      '#8b8b8b',
+      '#848484',
+      '#717171',
+      '#656565',
+      '#575757',
+    ],
+    dark: [
+      '#D9D9D9',
+      '#CCCCCC',
+      '#BFBFBF',
+      '#B3B3B3',
+      '#404040',
+      '#262626',
+      '#191919',
+      '#0D0D0D',
+      '#000002',
+      '#000000',
+    ],
+  },
+  components: {
+    Button: Button.extend({
+      classNames: buttonClasses,
+    }),
+  },
   /* Put your mantine theme override here */
 });
